b5b346a3c32852cbb6a9eeae497a2f0c682a66ef
Pat Tul <patchatulyakul@gmail.com>
2024-10-15 16:52:53 -0400

    add testScore if-else 3.2

---
+++ python3/testScore3_2.py
@@ -0,0 +1,32 @@
+#input testScore, classRank in terminal as string
+testScoreString = input("Enter test score: ")
+classRankString = input("Enter class rank: ")
+
+#convert string inputs to integer
+testScore = int(testScoreString)
+classRank = int(classRankString)
+
+#Unit Tesst 1 testScore 87 & classRank 60 Accept
+#Unit Test 2  testScore 60 & classRank 87 Reject
+if testScore >= 90:
+    if classRank >= 25:
+        print("Accept")
+    else:
+        print("Reject")
+else:
+    if testScore >= 80:
+        if classRank >= 50:
+            print("Accept")
+        else:
+            print("Reject")
+    else:
+        if testScore >= 70:
+            if classRank >= 75:
+                print("Accept")
+            else:
+                print("Reject")
+        else:
+            print("Reject")
+
+  
+
