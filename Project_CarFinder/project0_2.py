db196f2905cac17f41d1af5b74af5813f5325220
Pat Tul <patchatulyakul@gmail.com>
2024-11-05 00:38:00 -0500

    Add CarFinder 0_2 project

--- Project_CarFinder/project0_2.py
+++ Project_CarFinder/project0_2.py
@@ -1,4 +1,3 @@
-
 AllowedVehiclesList = ['Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan']
 
 menu = """********************************
@@ -6,19 +5,27 @@ AutoCountry Vehicle Finder v0.1
 ********************************
 Please Enter the following number below from the following menu:
 1. PRINT all Authorized Vehicles
-2. Exit
+2. SEARCH for Authorized Vehicle
+3. Exit
 """
-#unit test1
-print(menu)
-choose_menu = int(input("Enter Here:"))
 
-#unit test2
+print(menu)
+choose_menu = int(input("Enter Here: "))
+
 if choose_menu == 1:
     print("The AutoCountry sales manager has authorized the purchase and selling of the following vehicles: ")
     for i in AllowedVehiclesList:
         print(i)
-#unit test3
+#unit test1
 elif choose_menu == 2:
+    search_vehicle = input("Please Enter the full Vehicle name: ")
+#unit test2
+    if search_vehicle in AllowedVehiclesList:
+        print(search_vehicle + " is an authorized vehicle")
+#unit test3
+    else:
+        print(search_vehicle + " is not an authorized vehicle, if you received this in error please check the spelling and try again")
+elif choose_menu == 3:
     print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
 
-# multiple lines use """
+
