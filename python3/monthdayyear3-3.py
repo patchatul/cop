3d02e3885548048df5a999a9bf489c8cbc2f5aa1
Pat Tul <patchatulyakul@gmail.com>
2024-11-11 17:12:25 -0500

    Modify monthdayyear3 + Add unit test 3

--- python3/monthdayyear3-3.py
+++ python3/monthdayyear3-3.py
@@ -1,17 +1,29 @@
-month = int(input("Enter month: "))
-day = int(input("Enter day: "))
-year = int(input("Enter year: "))
+month = (input("Enter month: "))
+day = (input("Enter day: "))
+year = (input("Enter year: "))
 
 validDate = True
+MIN_YEAR = 0
+MIN_MONTH = 1
+MAX_MONTH = 12
+MIN_DAY = 1
+MAX_DAY = 31
 
-if year <= 0:
-    validDate = False
-elif month < 1 or month > 12:
-    validDate = False
-elif day < 1 or day > 31:
-    validDate = False
+def DetermineInvalidMonthFromDay(month):
+    days = [31,28,31,30,31,30,31,31,30,31,30,31]
+    if ((int(year)% 4) ==0):
+        days[1]=29
+    return days[int(month)-1]
+MAX_DAY =DetermineInvalidMonthFromDay(month)
+
+if int(year) <= MIN_YEAR: # invalid year
+  validDate = False
+elif int(month) < MIN_MONTH or int(month) > MAX_MONTH: # invalid month
+  validDate = False
+elif int(day) < MIN_DAY or int(day) > MAX_DAY: # invalid day
+  validDate = False
 
 if validDate == True:
-    print(str(month) +"/" + str(day) + "/" + str(year) + " is a valid date")
+      print(f"{month}/{day}/{year} is a valid date.")
 else:
-    print(str(month) +"/" + str(day) + "/" + str(year) + " is an invalid date")
+      print(f"{month}/{day}/{year} is an invalid date.")
