5bf6e5174e2563a1bac6387a7d01b0b72c224f1d
Pat Tul <patchatulyakul@gmail.com>
2024-11-11 19:52:45 -0500

    Add project0_4 Remove

--- Project_CarFinder/project0_4.py
+++ Project_CarFinder/project0_4.py
@@ -1,13 +1,14 @@
-AllowedVehiclesList = ['Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan']
+AllowedVehiclesList = ['Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan', 'Rivian R1T', 'Ram 1500']
 
 menu = """********************************
-AutoCountry Vehicle Finder v0.3
+AutoCountry Vehicle Finder v0.4
 ********************************
 Please Enter the following number below from the following menu:
 1. PRINT all Authorized Vehicles
 2. SEARCH for Authorized Vehicle
 3. ADD Authorized Vehicle
-4. Exit
+4. DELETE Authorized Vehicle
+5. Exit
 ********************************"""
 
 while True :
@@ -24,12 +25,18 @@ while True :
             print(search_vehicle + " is an authorized vehicle")
         else:
             print(search_vehicle + " is not an authorized vehicle, if you received this in error please check the spelling and try again")
-    #unit test 1
     elif choose_menu == "3":
-        new_vehicle = input("Please Enter the full Vehicle name you would like to add: ")
-    #unit test 2-Rivian R1T 3-Ram 1500  
+        new_vehicle = input("Please Enter the full Vehicle name you would like to add: ") 
         AllowedVehiclesList.append(new_vehicle)
         print(f'You have added "{new_vehicle}" as an authorized vehicle')
-    elif choose_menu == "4":
+#unit test 1  
+    elif choose_menu == "4": 
+        remove_vehicle = input("Please Enter the full Vehicle name you would like to REMOVE: ")
+        sure_to_remove = input(f'Are you sure you want to remove "{remove_vehicle}" from the Authorized Vehicles List? (yes/no) ')
+#unit test 2 3 
+        if sure_to_remove.lower() == "yes" and remove_vehicle in AllowedVehiclesList:
+            AllowedVehiclesList.remove(remove_vehicle)
+            print(f'You have REMOVED "{remove_vehicle}" as an authorized vehicle')
+    elif choose_menu == "5":
         print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
         break
